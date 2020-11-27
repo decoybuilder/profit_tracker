240c13a920ac66693e835e2a65d4c20c99d79691
Robert Tran <decoybuilder@hotmail.com>
2020-11-27 11:39:53 +1100

    commit before database write

--- asx_data.py
+++ asx_data.py
@@ -1,19 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-# source = requests.get('https://www.asx.com.au/asx/1/share/nab/').text
-
-# soup = BeautifulSoup(source, 'lxml')
-
-# data = soup.p.text
-
-# last_price_loc = data.find('last_price')
-# start = data.find(':', last_price_loc) + 1
-# end = data.find(',', start)
-# print(start, end)
-
-# print(data[start:end])
-
 def last_price(code):
     address = 'https://www.asx.com.au/asx/1/share/' + code + '/'
     source = requests.get(address).text
@@ -25,6 +12,4 @@ def last_price(code):
     start = data.find(':', last_price_loc) + 1
     end = data.find(',', start)
     
-    return data[start:end]
-
-print(type(float(last_price('nab'))))
+    return float(data[start:end])
--- profit_tracker.py
+++ profit_tracker.py
@@ -2,13 +2,15 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 import pandas as pd
-
+from asx_data import last_price
 
 # %%
 movement = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Movement')
 dividend = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Dividend')
 cash = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Cash')
 
+# %%
+cash_tot = sum(cash['Credit']) - sum(cash['Debit'])
 
 # %%
 sells = movement[movement['Action'] == 'Sell']
@@ -26,7 +28,7 @@ while len(sells) != 0:
     s_date, s_code, s_quantity, s_action, s_price, s_fees = sells.loc[sell_index]
 
     for buy_index in range(len(buys) - 1, -1, -1):
-        if (s_code == buys.loc[buy_index]['Code']) and (sells.loc[sell_index]['Date'] >= buys.loc[buy_index]['Date']):
+        if (s_code == buys.loc[buy_index, 'Code']) and (sells.loc[sell_index, 'Date'] >= buys.loc[buy_index, 'Date']):
             b_date, b_code, b_quantity, b_action, b_price, b_fees = buys.loc[buy_index]
 
             name = b_code.replace('.', '_').lower()
@@ -36,13 +38,13 @@ while len(sells) != 0:
             if s_quantity < b_quantity:
                 closed_profits_dict[name] += s_price * s_quantity - b_price * s_quantity - s_fees
                 buys.at[buy_index, 'Quantity'] -= s_quantity
-                sells.drop(sells.index[-1], inplace=True)
+                sells.drop(sell_index, inplace=True)
                 break
             
             elif s_quantity == b_quantity:
                 closed_profits_dict[name] += s_price * s_quantity - b_price *   b_quantity - s_fees - b_fees
                 buys.drop([buy_index], inplace=True)
-                sells.drop(sells.index[-1], inplace=True)
+                sells.drop(sell_index, inplace=True)
                 break
 
             else:
@@ -62,18 +64,35 @@ for div_index in range(0, len(dividend)):
         
     closed_profits_dict[name] += d_amount
 
-
 # %%
-cash_tot = sum(cash['Credit']) - sum(cash['Debit'])
+unclosed_profits_dict = {}
+buys.reset_index(drop=True, inplace=True)
+for buy_index in range(len(buys)):
+    b_date, b_code, b_quantity, b_action, b_price, b_fees = buys.loc[buy_index]
+    name = b_code.replace('.', '_').lower()
+
+    if name not in unclosed_profits_dict:
+        unclosed_profits_dict[name] = 0
+    
+    market_price = last_price(name.replace('_asx', ''))
+
+    unclosed_profits_dict[name] += market_price * b_quantity - b_quantity * b_price - b_fees
 
 
 # %%
 closed_profits = sum(closed_profits_dict.values())
-print(closed_profits_dict)
-print(closed_profits)
-
+unclosed_profits = sum(unclosed_profits_dict.values())
+total_profit = closed_profits + unclosed_profits
 
 # %%
+print(total_profit/cash_tot)
+
+# print(closed_profits_dict)
+# print(closed_profits)
+# print(unclosed_profits_dict)
+# print(unclosed_profits)
+
+
 
 
 
