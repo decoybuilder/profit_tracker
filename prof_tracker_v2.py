ccec98e8ecd7773cac52d8d3e75941eaa4979e80
Robert Tran <decoybuilder@hotmail.com>
2021-06-30 17:18:28 +1000

    RC version of profit tracker

--- prof_tracker_v2.py
+++ prof_tracker_v2.py
@@ -12,6 +12,7 @@ def is_year(start, end):
 movement = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Movement')
 dividend = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Dividend')
 cash = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Cash')
+exchange = pd.read_excel('C:\\Users\\decoy\\Documents\\Finance\\transactions.xlsx', sheet_name='Exchange')
 movement['Cost'] = [movement['Price'][idx] - movement['Fees'][idx] / movement['Quantity'][idx] if i == 'Sell' else movement['Price'][idx] + movement['Fees'][idx] / movement['Quantity'][idx] for idx, i in enumerate(movement['Action'])]
 
 
@@ -21,7 +22,7 @@ buys = movement[movement['Action'] == 'Buy'][::-1]
 buys.reset_index(drop=True, inplace=True)
 
 closed_profits = pd.DataFrame(columns=['Date', 'Code', 'Profit', 'Profit (%)', 'Type'])
-open_profits = pd.DataFrame(columns=['Code', 'Quantity', 'Amount'])
+open_profits = pd.DataFrame(columns=['Code', 'Quantity', 'Cost'])
 tax = pd.DataFrame(columns=['Date', 'Code', 'Amount'])
 
 for b_index, b_row in buys.iterrows():
@@ -32,8 +33,8 @@ for b_index, b_row in buys.iterrows():
         
 
         if s_quantity <= b_quantity:
-            profit = s_quantity * (s_cost * s_exchange - b_cost * b_exchange)
-            profit_p = 100 * (s_cost * s_exchange - b_cost * b_exchange)/(b_cost * b_exchange)
+            profit = s_quantity * (s_cost / s_exchange - b_cost/ b_exchange)
+            profit_p = 100 * (s_cost / s_exchange - b_cost / b_exchange)/(b_cost / b_exchange)
             new_row = {'Date': s_date, 'Code': b_code, 'Profit': profit, 'Profit (%)': profit_p, 'Type': 'Capital Gains'}
             closed_profits = closed_profits.append(new_row, ignore_index=True)
 
@@ -46,8 +47,8 @@ for b_index, b_row in buys.iterrows():
 
 
         elif s_quantity > b_quantity:
-            profit = b_quantity * (s_cost * s_exchange - b_cost * b_exchange)
-            profit_p = 100 * (s_cost * s_exchange - b_cost * b_exchange)/(b_cost * b_exchange)
+            profit = b_quantity * (s_cost / s_exchange - b_cost / b_exchange)
+            profit_p = 100 * (s_cost / s_exchange - b_cost / b_exchange)/(b_cost / b_exchange)
             new_row = {'Date': s_date, 'Code': b_code, 'Profit': profit, 'Profit (%)': profit_p, 'Type': 'Capital Gains'}
             closed_profits = closed_profits.append(new_row, ignore_index=True)
 
@@ -60,6 +61,10 @@ for b_index, b_row in buys.iterrows():
 
         if b_quantity == 0: break
 
+    if b_quantity != 0:
+        o_row = {'Code': b_code, 'Quantity': b_quantity, 'Cost': b_cost / b_exchange}
+        open_profits = open_profits.append(o_row, ignore_index=True)
+
 for d_index, d_row in dividend.iterrows():
     d_date, d_code, d_amount, d_frank = d_row
     new_row = {'Date': d_date, 'Code': d_code, 'Profit': d_amount + d_frank , 'Profit (%)': 'N/A', 'Type': 'Dividend'}
@@ -70,6 +75,46 @@ for d_index, d_row in dividend.iterrows():
 
 close_profits_sum = closed_profits.groupby('Code', sort = False)['Profit'].sum().reset_index()
 tax_sum = tax.groupby('Code', sort = False)['Amount'].sum().reset_index()
-print(close_profits_sum )
+closed_profits_comb = close_profits_sum.merge(tax_sum, on='Code')
+closed_profits_comb.rename(columns = {'Amount':'Taxable'}, inplace=True)
+
+open_profits['Cost Base'] = open_profits['Cost'] * open_profits['Quantity']
+open_profits_sum = open_profits.groupby('Code', sort = False)['Quantity'].sum().reset_index()
+open_profits_sum = open_profits_sum.merge(open_profits.groupby('Code', sort = False)['Cost Base'].sum().reset_index(), on='Code')
+open_profits_sum['Avg Cost'] = open_profits_sum['Cost Base'] / open_profits_sum['Quantity']
+
+open_code = '^AXJO ' + ' '.join(open_profits_sum['Code'].tolist())
+
+data = yf.download(open_code, start=cash['Date'][0].date(), group_by='ticker')
+
+market_list = [0] * len(open_profits_sum['Code'])
+cache = {'AX':1}
+for idx, code in enumerate(open_profits_sum['Code']):
+    code_split = code.split('.')
+    if len(code_split) == 1: code_split.append('USA')
+        
+    cur = code_split[1]
+    try:
+        rate = cache[cur]
+
+    except:
+        ex_code = exchange.loc[exchange['Suffix'] == cur]['Code'].reset_index(drop=True)[0]
+        rate = yf.Ticker(ex_code).history(period='1d')['Close'][0]
+        cache[cur] = rate
+
+    market_list[idx] = x / rate if not pd.isna(x := data[code]['Close'][-1]) else data[code]['Close'][-2] / rate
+
+
+open_profits_sum['Market Price'] = market_list
+open_profits_sum['Profit'] = (open_profits_sum['Market Price'] - open_profits_sum['Avg Cost']) * open_profits_sum['Quantity']
+open_profits_sum['Profit (%)'] = 100 * (open_profits_sum['Market Price'] - open_profits_sum['Avg Cost']) / open_profits_sum['Avg Cost']
+
+
+print('Closed Profits')
+print(closed_profits_comb)
 print(f'The total amount of closed profit is ${close_profits_sum["Profit"].sum()}')
 print(f'The amount to be added to your income is ${tax_sum["Amount"].sum()}')
+print('\n')
+print('Open Profits')
+print(open_profits_sum)
+print(f'The total amount of open profit is ${open_profits_sum["Profit"].sum()}')
